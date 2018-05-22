026ac88caea00497c3f9aa60e286f0797d46ed62
ivan_weng <ivan_weng@tw.shuttle.com>
2018-05-22 13:25:39 +0800

    add sidebar

--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -1,41 +1,36 @@
 @extends('layouts.base')
 
 @section('content')
-    <div class="container">
-        @auth
-            <div class="row justify-content-center">
-                <div class="col-10">
-                    <a class="btn btn-primary" href="{{ url('user/groups/create') }}" role="button">Create Group</a>
-                </div>    
-            </div>
-            <br>
-        @endauth
-        @foreach ($groups as $group)
-            <div class="row justify-content-center">
-                <div class="col-8">
-                    <div class="card">
-                        <a href="{{ url('groups/'.$group->id) }}"><h5 class="card-header">{{ $group->title }}</h5></a>
-                        <div class="card-body">
-                            <p class="card-text">{{ $group->description }}</p>
-                        </div>
+    @auth
+        <div class="row">
+            <div class="col-10">
+                <a class="btn btn-primary" href="{{ url('user/groups/create') }}" role="button">Create Group</a>
+            </div>    
+        </div>
+        <br>
+    @endauth
+    @foreach ($groups as $group)
+        <div class="row mt-2">
+            <div class="col-8">
+                <div class="card">
+                    <a href="{{ url('groups/'.$group->id) }}"><h5 class="card-header">{{ $group->title }}</h5></a>
+                    <div class="card-body">
+                        <p class="card-text">{{ $group->description }}</p>
                     </div>
                 </div>
-                <div class="col-2 align-self-center">
-                    @auth
-                        @if ($group->user_id == Auth::user()->id)
-                            <a class="btn btn-success" href="{{ url('user/groups/'.$group->id.'/edit') }}" role="button">Edit</a>
-                            <form action="{{ url('user/groups/'.$group->id) }}" method="POST" style="display: inline;">
-                                {{ method_field('DELETE') }}
-                                {{ csrf_field() }}
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>                        
-                        @endif
-                    @endauth
-                </div>            
-            </div>        
-            @if(!$loop->last)
-                <br>
-            @endif        
-        @endforeach
-    </div>
+            </div>
+            <div class="col-2 align-self-center">
+                @auth
+                    @if ($group->user_id == Auth::user()->id)
+                        <a class="btn btn-success" href="{{ url('user/groups/'.$group->id.'/edit') }}" role="button">Edit</a>
+                        <form action="{{ url('user/groups/'.$group->id) }}" method="POST" style="display: inline;">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>                        
+                    @endif
+                @endauth
+            </div>            
+        </div>      
+    @endforeach
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,53 +22,90 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{ url('groups') }}">Home <span class="sr-only">(current)</span></a>
-                    </li>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ url('groups') }}">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    User
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('user/groups') }}">Groups</a>
+                    </div>
+                </li>
+            </ul>  
+            <ul class="navbar-nav ml-auto">
+                @guest
+                    <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                    <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                @else
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        User
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
+
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ url('user/groups') }}">Groups</a>
-                        </div>
-                    </li>
-                </ul>  
-                <ul class="navbar-nav ml-auto">
-                    @guest
-                        <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                        <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
                             </a>
 
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>                      
-            </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>                      
         </div>
     </nav>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-2 d-none d-sm-block navbar-dark bg-secondary">
+                <ul class="navbar-nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Reports</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Analytics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Export</a>
+                    </li>                    
+                </ul>
+                <ul class="navbar-nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Reports</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Analytics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Export</a>
+                    </li>                    
+                </ul>                
+            </nav>
+            <main class="col-10 py-4">
+                @yield('content')
+            </main>            
+        </div>
+    </div>
+
+
     @if (count($errors) > 0)
         <div class="alert alert-danger alert-dismissible fade show">
             {!! implode('<br>', $errors->all()) !!}
@@ -86,8 +123,6 @@
             </button>                 
         </div>
     @endif
-    <main class="py-4">
-        @yield('content')
-    </main>
+
 </body>
 </html>
